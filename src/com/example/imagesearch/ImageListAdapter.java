e735b5e52eacfb87b3f4fc66f70391bcfbdc0bbf
Louis <louis1204@gmail.com>
2014-02-20 16:33:28 -0800

    final
    Was able to search and receive the json result. Wasn't able to load the
    result into view.

--- src/com/example/imagesearch/ImageListAdapter.java
+++ src/com/example/imagesearch/ImageListAdapter.java
@@ -1,65 +1,59 @@
 package com.example.imagesearch;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class ImageListAdapter extends ArrayAdapter<ImageModel>{
+public class ImageListAdapter extends BaseAdapter{
     
 	ImageModel item;
-	private final LayoutInflater mInflater;
-    
-    public ImageListAdapter(Context context) {
-    	super(context, R.layout.image_list_row);
-    	mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	private static LayoutInflater mInflater;
+    ArrayList<HashMap<String, String>> data;
+    public ImageListAdapter(Activity activity, ArrayList<HashMap<String,String>> data) {
+    	this.data = data;
+    	mInflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
     
-    public void setData(List<ImageModel> data) {
-        clear();
-        if (data != null) {
-            for (ImageModel appEntry : data) {
-                add(appEntry);
-            }
-        }
+    public int getCount() {
+        return data.size();
     }
-    
-    static class ViewHolder {
-        protected Button chat;
-        protected Button profile;
-        protected Button delete;
-        protected Button cancel;
-      }
+ 
+    public Object getItem(int position) {
+        return position;
+    }
+ 
+    public long getItemId(int position) {
+        return position;
+    }
     
     /**
      * Populate new items in the list.
      */
     @Override 
     public View getView(int position, final View convertView, ViewGroup parent) {
-        View view = null;
-        item = getItem(position);
+        View view = convertView;
         
         //We currently have no view
         if (convertView == null) {
-            view = mInflater.inflate(R.layout.image_list_row, parent, false);	//set the view to a friend list row
+            view = mInflater.inflate(R.layout.image_list_row, null);	//set the view to a friend list row
 
-            if(item.getTitle() != "null")
-            	((TextView)view.findViewById(R.id.title)).setText(item.getTitle());	//set the mood if it's not null
-            else
-            	((TextView)view.findViewById(R.id.title)).setText("");
-        
-        } 
-        
-        else {
-        	//return the same view if it's not selected
-        	view = convertView;
         }
         
+        TextView title = (TextView)view.findViewById(R.id.title);
+        
+        HashMap<String, String> pic = new HashMap<String, String>();
+        
+        title.setText(pic.get("title"));
         return view;
     }
 }
--- src/com/example/imagesearch/MainActivity.java
+++ src/com/example/imagesearch/MainActivity.java
@@ -5,18 +5,16 @@ import java.util.HashMap;
 import java.util.List;
 
 import json.JSONParser;
+
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.Activity;
 import android.app.ListActivity;
-import android.app.LoaderManager;
 import android.app.ProgressDialog;
-import android.content.AsyncTaskLoader;
-import android.content.Context;
-import android.content.Loader;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,24 +22,22 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends ListActivity implements LoaderManager.LoaderCallbacks<List<ImageModel>>{
+public class MainActivity extends Activity {
 	EditText searchQuery;
 	Button searchButton;
 	private ProgressDialog pDialog;
 	JSONParser jp = new JSONParser();
 	ImageListAdapter imageListAdapter;
 	static ArrayList<HashMap<String,String>> imageList = new ArrayList<HashMap<String,String>>();
-
+	ListView list;
 	static String urlImages = "http://ajax.googleapis.com/ajax/services/search/images";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		imageListAdapter = new ImageListAdapter(this);
-		setListAdapter(imageListAdapter);
 		
 		searchQuery = (EditText) findViewById(R.id.search_field);
 		searchButton = (Button) findViewById(R.id.search_button);	
@@ -64,7 +60,9 @@ public class MainActivity extends ListActivity implements LoaderManager.LoaderCa
 	
 	void initLoader()
 	{
-		getLoaderManager().initLoader(0, null, this);
+		list = (ListView)findViewById(R.id.list);
+		imageListAdapter = new ImageListAdapter(this, imageList);
+		list.setAdapter(imageListAdapter);
 	}
 	
 
@@ -130,141 +128,5 @@ public class MainActivity extends ListActivity implements LoaderManager.LoaderCa
            // dismiss the dialog once product deleted
            pDialog.dismiss();
        }	
-	}
-
-
-	@Override
-	public Loader<List<ImageModel>> onCreateLoader(int arg0, Bundle arg1) {
-		return new DataListLoader(this);
-	}
-
-
-	@Override
-	public void onLoadFinished(Loader<List<ImageModel>> arg0, List<ImageModel> data) {
-		imageListAdapter.setData(data);
-	}
-
-
-	@Override
-	public void onLoaderReset(Loader<List<ImageModel>> arg0) {
-		imageListAdapter.setData(null);
-		
-	}
-	
-	public static class DataListLoader extends AsyncTaskLoader<List<ImageModel>> {
-		List<ImageModel> mModels;
- 
-        public DataListLoader(Context context) {
-            super(context);
-        }
- 
-        @Override
-        public List<ImageModel> loadInBackground() {
-            System.out.println("DataListLoader.loadInBackground");
-             
-             // You should perform the heavy task of getting data from 
-             // Internet or database or other source 
-             // Here, we are generating some Sample data
- 
-            // Create corresponding array of entries and load with data.
-            List<ImageModel> entries = new ArrayList<ImageModel>();
-            for(int i = 0; i < imageList.size(); i++)
-            {
-            	entries.add(new ImageModel(imageList.get(i).get("title"),
-            			imageList.get(i).get("url")));
-            }
- 
-            return entries;
-        }
-         
-        /**
-         * Called when there is new data to deliver to the client.  The
-         * super class will take care of delivering it; the implementation
-         * here just adds a little more logic.
-         */
-        @Override public void deliverResult(List<ImageModel> listOfData) {
-            if (isReset()) {
-                // An async query came in while the loader is stopped.  We
-                // don't need the result.
-                if (listOfData != null) {
-                    onReleaseResources(listOfData);
-                }
-            }
-            List<ImageModel> oldApps = listOfData;
-            mModels = listOfData;
- 
-            if (isStarted()) {
-                // If the Loader is currently started, we can immediately
-                // deliver its results.
-                super.deliverResult(listOfData);
-            }
- 
-            // At this point we can release the resources associated with
-            // 'oldApps' if needed; now that the new result is delivered we
-            // know that it is no longer in use.
-            if (oldApps != null) {
-                onReleaseResources(oldApps);
-            }
-        }
- 
-        /**
-         * Handles a request to start the Loader.
-         */
-        @Override protected void onStartLoading() {
-            if (mModels != null) {
-                // If we currently have a result available, deliver it
-                // immediately.
-                deliverResult(mModels);
-            }
- 
- 
-            if (takeContentChanged() || mModels == null) {
-                // If the data has changed since the last time it was loaded
-                // or is not currently available, start a load.
-                forceLoad();
-            }
-        }
- 
-        /**
-         * Handles a request to stop the Loader.
-         */
-        @Override protected void onStopLoading() {
-            // Attempt to cancel the current load task if possible.
-            cancelLoad();
-        }
- 
-        /**
-         * Handles a request to cancel a load.
-         */
-        @Override public void onCanceled(List<ImageModel> apps) {
-            super.onCanceled(apps);
- 
-            // At this point we can release the resources associated with 'apps'
-            // if needed.
-            onReleaseResources(apps);
-        }
- 
-        /**
-         * Handles a request to completely reset the Loader.
-         */
-        @Override protected void onReset() {
-            super.onReset();
- 
-            // Ensure the loader is stopped
-            onStopLoading();
- 
-            // At this point we can release the resources associated with 'apps'
-            // if needed.
-            if (mModels != null) {
-                onReleaseResources(mModels);
-                mModels = null;
-            }
-        }
- 
-        /**
-         * Helper function to take care of releasing resources associated
-         * with an actively loaded data set.
-         */
-        protected void onReleaseResources(List<ImageModel> apps) {}
 	}
 }
